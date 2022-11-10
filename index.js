5f679709bfe923713ce42045060831ee307a36bf
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-11-10 15:31:48 +0600

    user and pass set

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 require('dotenv').config();
 
@@ -10,6 +11,24 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// database name : serviceReviewDBUser
+//password: FfJaZg0t3s6Vc3d4
+
+ 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yagkdpa.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
+
+
 
 app.get('/', (req, res) => {
     

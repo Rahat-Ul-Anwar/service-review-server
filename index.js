a8531aa4288a8171d7d1aa5541ec1241fb772921
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-11-10 18:23:36 +0600

    get request done

--- index.js
+++ index.js
@@ -12,20 +12,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// database name : serviceReviewDBUser
-//password: FfJaZg0t3s6Vc3d4
+
 
  
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.yagkdpa.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+ 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
 
+    try {
+        const serviceCollection = client.db('serviceReview').collection('services');
+
+        app.get('/services', async (req, res) => {
+            
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+        
+
+    }
+   finally{} 
+
+}
+
+run().catch(error => console.error('error'));
 
 
 
@@ -40,3 +53,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('service review server is running on port', port);
 })
+
+
+
+
